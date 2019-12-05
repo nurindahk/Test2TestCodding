56e6cef339df014f4b6de950bd02996955a74e71
Nur Indah Komalasari Dewi <nurindah407203@gmail.com>
2019-12-05 14:27:46 +0700

    menambahkan dua angka

---
+++ helloWorld/DitambahDuaAngka.java
@@ -0,0 +1,5 @@
+public class DitambahDuaAngka{
+    public static void main(String[] args){
+        System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
+    }
+}
--- helloWorld/helloWorld.java
+++ helloWorld/helloWorld.java
@@ -1,5 +1,23 @@
 public class helloWorld{
-    public static void main(String[] args){
+    public static void main(final String[] args) {
         System.out.println("Hello World !");
+        mobil bmw = new mobil(); //kita bangun object mobil bmw
+        bmw.merk = "BMW i3000";
+        bmw.maju();
+
+        mobil fordShelby = new mobil ();
+        fordShelby.merk = "Ford Mustang Shelby";
+        fordShelby.maju();
+
+        System.out.println(args[0]+args[1]);
+    }
+}
+
+
+class mobil{
+    public String merk;
+
+    public void maju(){
+        System.out.println("Mobil "+this.merk+" maju !!!! Brmmm....");
     }
 }
