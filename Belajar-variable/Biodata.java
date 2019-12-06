dc43dc3188e3fc9f193ae48b6723e3404a831f65
Nur Indah Komalasari Dewi <nurindah407203@gmail.com>
2019-12-06 15:20:00 +0700

    tambah biodata

---
+++ Belajar-variable/Biodata.java
@@ -0,0 +1,16 @@
+public class Biodata{
+    public static void main(String[] args){
+        String nama = "Nur Indah Komalasari Dewi";
+        String tempat ="Tangerang";
+        int tanggal = 11;
+        int bulan = 05;
+        int tahun = 1998;
+        double umur =  21.5;
+        String alamat = "Kedokan cipete kecamatan cisauk kabupaten tangerang";
+
+        System.out.println(nama);
+        System.out.println(tempat+-+tanggal+-++bulan+-+tahun);
+        System.out.println(umur);
+        System.out.println(alamat);
+    }
+}
